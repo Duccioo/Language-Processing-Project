ce2a00cb0b6a5ac72c8f11a4f65993ab2c149c57
Duccioo <meconcelliduccio@hotmail.it>
2024-01-17 19:27:13 +0100

    update telegram bot

--- NLP/script/inference.py
+++ NLP/script/inference.py
@@ -43,6 +43,7 @@ def main():
         model=model_name,
         tokenizer=model_name,
         chunk_length_s=30,
+        generate_kwargs={"task": "transcribe", "language": "<|it|>"},
     )
     speech, sr = librosa.load(audio_file_name, sr=16000)
     transcribed_speech = transcriber(speech, batch_size=8)
--- NLP/script/test_telegram.py
+++ NLP/script/test_telegram.py
@@ -10,7 +10,13 @@ from transformers import (
     Wav2Vec2Processor,
     WhisperProcessor,
     WhisperForConditionalGeneration,
+    AutoModelForSeq2SeqLM,
+    AutoTokenizer,
 )
+
+
+# from transformers import
+# from transformers import AutoTokenizer
 
 # from datasets import Audio, load_dataset
 
@@ -30,6 +36,17 @@ bot = telebot.TeleBot(TOKEN)
 
 
 def download_content(url):
+    """
+    Download the content from a given URL.
+
+    Parameters:
+    - url (str): The URL to download the content from.
+
+    Returns:
+    - content (bytes): The content downloaded from the URL.
+
+    - None: If the download encountered an error.
+    """
     try:
         # Effettua la richiesta GET al link
         response = requests.get(url)
@@ -47,8 +64,9 @@ def download_content(url):
         return None
 
 
-# Funzione per ottenere la trascrizione del file audio
 def transcribe_audio_base(audio_data):
+    # Funzione per ottenere la trascrizione del file audio
+
     recognizer = sr.Recognizer()
 
     # Converte i dati audio in formato WAV
@@ -74,45 +92,90 @@ def transcribe_audio_base(audio_data):
 def transcribe_audio_NLP(audio_data):
     # model_name = "dbdmg/wav2vec2-xls-r-300m-italian"
 
-    model_name = "openai/whisper-tiny"
+    model_name = "results/model/ctc_common_11_7000"
+    # model_type = 3
 
-    model_type = 2
+    speech, sr = librosa.load(io.BytesIO(audio_data), sr=16000)
 
-    if model_type == 1:
-        wav2vec2_processor = Wav2Vec2Processor.from_pretrained(model_name)
-        wav2vec2_model = Wav2Vec2ForCTC.from_pretrained(model_name)
+    # if model_type == 1:
+    #     # ctc
+    #     wav2vec2_processor = Wav2Vec2Processor.from_pretrained(model_name)
+    #     wav2vec2_model = Wav2Vec2ForCTC.from_pretrained(model_name)
 
-        speech, sr = librosa.load(io.BytesIO(audio_data), sr=16000)
-        input_values = wav2vec2_processor(
-            speech, sampling_rate=16000, return_tensors="pt"
-        )
+    #     input_values = wav2vec2_processor(
+    #         speech, sampling_rate=16000, return_tensors="pt"
+    #     )
 
-        with torch.no_grad():
-            logits = wav2vec2_model(**input_values).logits
-        pred_ids = torch.argmax(logits, dim=-1)
-        transcribed_speech = wav2vec2_processor.batch_decode(pred_ids)
+    #     with torch.no_grad():
+    #         logits = wav2vec2_model(**input_values).logits
+    #     pred_ids = torch.argmax(logits, dim=-1)
+    #     transcribed_speech = wav2vec2_processor.batch_decode(pred_ids)[0]
 
-    elif model_type == 2:
-        # seq2seq
-        processor = WhisperProcessor.from_pretrained(model_name)
-        model = WhisperForConditionalGeneration.from_pretrained(model_name)
-        forced_decoder_ids = processor.get_decoder_prompt_ids(
-            language="italian", task="transcribe"
-        )
-        speech, sr = librosa.load(io.BytesIO(audio_data), sr=16000)
+    # elif model_type == 2:
+    #     # seq2seq
+    #     processor = WhisperProcessor.from_pretrained(model_name)
+    #     model = WhisperForConditionalGeneration.from_pretrained(model_name)
 
-        input_features = processor(
-            speech, sampling_rate=16000, return_tensors="pt"
-        ).input_features
+    #     forced_decoder_ids = processor.get_decoder_prompt_ids(
+    #         language="italian", task="transcribe"
+    #     )
+    #     input_features = processor(
+    #         speech, sampling_rate=16000, return_tensors="pt"
+    #     ).input_features
 
-        predicted_ids = model.generate(
-            input_features, forced_decoder_ids=forced_decoder_ids
-        )
-        transcribed_speech = processor.batch_decode(
-            predicted_ids, skip_special_tokens=True
-        )
+    #     predicted_ids = model.generate(
+    #         input_features, forced_decoder_ids=forced_decoder_ids
+    #     )
+    #     transcribed_speech = processor.batch_decode(
+    #         predicted_ids, skip_special_tokens=True
+    #     )[0]
 
-    return transcribed_speech[0]
+    # else:
+
+    transcriber = pipeline(
+        "automatic-speech-recognition",
+        model=model_name,
+        tokenizer=model_name,
+        chunk_length_s=30,
+        generate_kwargs={"task": "transcribe", "language": "<|it|>"},
+    )
+    transcribed_speech = transcriber(speech, batch_size=8)["text"]
+
+    # print("**" * 5, transcribed_speech, "**" * 5)
+
+    return transcribed_speech
+
+
+def summary(text):
+    """
+    Generates a summary of the given text using the T5 model for Italian summarization.
+
+    Parameters:
+        text (str): The text to be summarized.
+
+    Returns:
+        str: The generated summary of the text.
+    """
+    T5_model_ita_base = "efederici/it5-base-summarization"
+
+    tokenizer = AutoTokenizer.from_pretrained(T5_model_ita_base)
+
+    inputs = tokenizer(text, return_tensors="pt")
+
+    model = AutoModelForSeq2SeqLM.from_pretrained(T5_model_ita_base)
+
+    outputs = model.generate(
+        inputs["input_ids"],
+        max_length=50,
+        # min_length=10,
+        length_penalty=1.80,
+        num_beams=2,
+        # early_stopping=True,
+    )
+
+    return tokenizer.decode(
+        outputs[0], clean_up_tokenization_spaces=True, skip_special_tokens=True
+    )
 
 
 # Gestione del messaggio vocale
@@ -130,15 +193,18 @@ def handle_voice_message(message):
 
     # Ottieni la trascrizione
     transcription = transcribe_audio_NLP(audio_data)
-    transcription2 = transcribe_audio_base(audio_data)
+    # transcription2 = transcribe_audio_base(audio_data)
+
+    summary_text = summary(transcription)
 
     # Invia la trascrizione come risposta
     bot.reply_to(
         message,
-        f"Trascrizione con modelli addestrati: {transcription}\n\n--------\nTrascrizione con modello Google: {transcription2}",
+        f"🎙️ <b>Transcription</b>: {transcription}\n\n📚 <b>Summary</b>: {summary_text}",
+        parse_mode="HTML",
     )
 
 
 # Avvia il bot
 if __name__ == "__main__":
-    bot.polling(none_stop=True)
+    bot.polling(non_stop=True)
